2de8dbd7cf1af63fbf447516a9ab17db3286908a
DimaDzh <dzhartyds@gmail.com>
2023-01-13 16:07:21 +0200

    was create basic store

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App";
 
+import { BrowserRouter } from "react-router-dom";
+import store from "./store";
+import { Provider } from "react-redux";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/store/index.ts
@@ -0,0 +1,22 @@
+import { configureStore, compose, combineReducers } from "@reduxjs/toolkit";
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+  composeEnhancers,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
